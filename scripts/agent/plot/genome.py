82bff5ba75577bc1a0e7e5c2afe609cc70a41c43
Jaimie Murdock <jaimie.murdock@gmail.com>
2011-10-28 18:57:01 -0400

    added documentation

--- scripts/agent/plot/genome.py
+++ scripts/agent/plot/genome.py
@@ -28,43 +28,60 @@ import numpy as np
 from math import sqrt
 from agent.genome import get_label
 
-def plot(genes, filename="plot.png", func=lambda a: a.id, 
-         plot_title='', cmap='Paired', filter=lambda a: True, 
+def plot(genes, filename="plot.png", plot_title='', cmap='Paired', 
          draw_legend=False):
+    # get population
     p = get_population()
+
+    # initialize population count
     pops = [[] for i in xrange(30000)]
-
-    cmap = pylab.cm.jet
-
     for a in p:
         for step in range(a.birth, a.death):
             pops[step].append(a)
 
+    # initialize plot variables
+    cmap = pylab.cm.jet
     lines=[]
-    for i, gene in enumerate(genes):
-        color = cmap(float(i)/len(genes))
 
+    # build plot for each gene
+    for i, gene in enumerate(genes):
+
+        # initialize line variables
         data = []
         err_up = []
         err_down = []
+        
+        # calculate points for each time step
         for step in xrange(30000):
             datum = [agent.genome[gene] for agent in pops[step]]
             mean = sum(datum) / len(pops[step])
             stderr = np.std(datum) / sqrt(len(pops[step]))
 
-            #err_up.append(min(mean+stderr, 255))
-            #err_down.append(max(mean-stderr,0))
+            # calculate 1 std dev error bars
             err_up.append(min(mean+np.std(datum), 255))
             err_down.append(max(mean-np.std(datum),0))
             data.append(mean)
+            
+            # TODO: Implement stderr/stddev toggle
+            """
+            # calculate 1 std err error bars
+            err_up.append(min(mean+stderr, 255))
+            err_down.append(max(mean-stderr,0))
+            """
+        
+        color = cmap(float(i)/len(genes))
+
+        # use error bars
         fill = pylab.fill_between(range(0,30000,5), err_up[::5], err_down[::5], 
                                   color=color, alpha=0.35, linewidth=0,
                                   edgecolor=None, facecolor=color)
-        #pylab.plot(range(30000), err_up, alpha=0.35, color=color)
-        #pylab.plot(range(30000), err_down, alpha=0.35, color=color)
+        # plot line
         line = pylab.plot(range(30000), data, color=color)
+
+        # append to list of lines for use with data legend
         lines.append(line)
 
+    # plot all the things
     pylab.title("Size and Internal Neural Group Count (INGC)", weight='black')
     pylab.ylim(0, 255)
     pylab.xlim(0, 30000)
